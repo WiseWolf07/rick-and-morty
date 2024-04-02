c8310049b1dc5a2cc82ae14b2e8b75b6fe1e5a7c
SmokeCode <miguelangel.velez@udea.edu.co>
2024-04-02 08:08:19 -0500

    v.1.0 rick-and-morty

--- src/app/components/Card/Card.js
+++ src/app/components/Card/Card.js
@@ -3,8 +3,21 @@ import { getCharacters } from "@/app/lib/characters";
 
 async function Card() {
   const characters = await getCharacters();
-  console.log(characters);
-  return <h2>Card</h2>;
+  const charactersData = characters.results;
+  return (
+    <div className="flex-container">
+      {charactersData.map((character, index) => (
+        <div className="card">
+          <h2 key={index}>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+          <ul>
+            <li><span>Status:</span> {character.status}</li>
+            <li><span>Species:</span> {character.species}</li>
+          </ul>
+        </div>  
+      ))}
+    </div>
+  );;
 }
 
 export default Card;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Card from "./components/Card/Card";
+import "./style.css";
 
 export default function Home() {
   return (
